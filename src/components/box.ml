3f4f399dccff704c015953898d00d25a40f1fa4f
erwan.lemattre <erwan.lemattre@universite-paris-saclay.fr>
2024-02-17 23:38:18 +0100

    Fix textures

--- src/components/box.ml
+++ src/components/box.ml
@@ -16,10 +16,15 @@ let create id x y w h mass settings =
     
 
     let surface_tmp = Gfx.create_surface ctx w h in
-    for i = 0 to (w/(settings.t_w*64))-1 do
-      Gfx.blit_full ctx surface_tmp res (settings.t_x*64) (settings.t_y*64) (settings.t_w*64)
-                   (settings.t_h*64) (i*64) 0 (settings.t_w*64) (settings.t_h*64);
-    done;
+
+    let rec loop i =
+      if i <= (w/(settings.t_w*64)*settings.t_w) then
+        (Gfx.blit_full ctx surface_tmp res (settings.t_x*64) (settings.t_y*64) (settings.t_w*64)
+        (settings.t_h*64) (i*64) 0 (settings.t_w*64) (settings.t_h*64);
+        Gfx.debug "i = %d\n%!" i; 
+        loop (i+settings.t_w));
+    in loop 0; 
+
     box # texture # set (Image surface_tmp)
     
     );
